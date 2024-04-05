62735fc1dbd423af463ed4267744d6f3e58b31a1
ShubhamVerma16 <46774613+ShubhamVerma16@users.noreply.github.com>
2024-04-05 15:10:41 +0530

    Create test.py

---
+++ model/test.py
@@ -0,0 +1,32 @@
+import torch.nn.functional as F
+import torch
+
+class Test:
+    def __init__(self):
+        pass
+
+    def test(self, model, device, criterion, test_loader):
+        model.eval()
+        test_loss = 0
+        correct = 0
+        with torch.no_grad():
+            for data, target in test_loader:
+                data, target = data.to(device), target.to(device)
+                output = model(data)
+                test_loss += F.cross_entropy(output, target, reduction="sum").item()
+                pred = output.argmax(dim=1, keepdim=True)
+                correct += pred.eq(target.view_as(pred)).sum().item()
+
+        test_loss /= len(test_loader.dataset)
+
+        print(
+            "\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n".format(
+                test_loss,
+                correct,
+                len(test_loader.dataset),
+                100.0 * correct / len(test_loader.dataset),
+            )
+        )
+        test_acc = 100.0 * correct / len(test_loader.dataset)
+
+        return test_loss, test_acc
